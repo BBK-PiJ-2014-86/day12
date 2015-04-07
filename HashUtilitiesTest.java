f8a864abe6b2f7243da15df205938062eb7e9c41
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-04-07 20:57:05 +0100

    correction
    increasing the pool from random number and taking the max number of
    1000 randomly generated numbers

--- HashUtilitiesTest.java
+++ HashUtilitiesTest.java
@@ -13,8 +13,16 @@ public class HashUtilitiesTest {
 	@Test
 	public void hashUtilitiesTest() {
 		
-		Random r = new Random ();
-		int rand = r.nextInt(1000000);
+		int rand = 0;
+		
+		// producing the max of 1000 instances of random
+		for (int i=0; i<1000;i++) {
+			Random r = new Random ();
+			int temp = r.nextInt(1000000);
+			if (rand<temp) rand=temp;
+		}
+		
+		// hash of the max number
 		rand= HashUtilities.shortHash(rand);
 		
 		assertTrue(rand <1000);
